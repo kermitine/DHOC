81480044e08286db86052a2953311f67fca849c5
Ayrik Nabirahni <ayriknabirahni01@gmail.com>
2025-08-05 17:37:06 -0700

    created variables

--- config/config.py
+++ config/config.py
@@ -1,10 +1,16 @@
 version = '0.0.3'
 
+# motor variables
+motor_rpm = 3000
+gear_ratio = 100
+stepper_motor_steps = 200
 
 
-prompt_input_not_recognized = 'Input not recognized. Please try again.'
 
 
-
+# positional variabkes
 dh_observatory_lat = 33.863907
 dh_observatory_lon = -118.255190
+
+# commonly used prompts
+prompt_input_not_recognized = 'Input not recognized. Please try again.'
